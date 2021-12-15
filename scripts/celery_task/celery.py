3a77d49f9a672d28c543210f8a67a76a60263a8b
kx <123@qq.com>
2021-12-15 23:55:18 +0800

    定时任务

--- scripts/celery_task/celery.py
+++ scripts/celery_task/celery.py
@@ -11,4 +11,33 @@ backend = 'redis://:333333@175.24.179.83:6379/2'
 
 # 这里把任务文件进行注册
 app = Celery(__name__, broker=broker, backend=backend
-             ,include=['celery_task.task_1','celery_task.task_2'])
+             , include=['celery_task.task_1', 'celery_task.task_2'])
+
+# 执行定时任务
+# 时区
+app.conf.timezone = 'Asia/Shanghai'
+# 是否使用utc
+app.conf.enable_utc = False
+
+# 任务定时配置
+from datetime import timedelta
+from celery.schedules import crontab
+
+# crontab(hour=8,day_of_week=1) 每周一早8点执行
+app.conf.beat_schedule = {
+    'add-task': {
+        'task': 'celery_task.task_1.add',
+        'schedule': timedelta(seconds=3),
+        'args': (300, 50)
+    }
+}
+"""
+之前的任务，都是需要代码去手动提交到worker
+同样定时任务，其实就是按照设定的周期用beat自动提交
+
+需要一个启动一个beat
+用来定时提交任务到worker
+
+启动beat：
+celery -A celery_task beat -l info
+"""
--- scripts/t_celery_add_task.py
+++ scripts/t_celery_add_task.py
@@ -2,10 +2,30 @@
 添加celery任务
 """
 from celery_task.task_1 import add
-
-id  = add.delay(1, 2)
+"""
+执行异步任务
+"""
+id = add.delay(1, 2)
 print(id)
 from celery_task.task_2 import add
 
 id2 = add.delay(2, 5)
 print(id2)
+
+"""
+执行延迟任务
+"""
+from celery_task.task_1 import add
+from datetime import datetime, timedelta
+
+# 表示十秒之后的时间
+eta = datetime.utcnow() + timedelta(seconds=10)
+
+# args 函数的参数，eta 延迟时间，utc时间
+add.apply_async(args=(200,5),eta=eta)
+
+
+"""
+执行定时任务
+在 主py里配置
+"""
