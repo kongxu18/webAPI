d01deeca5e34e726aa8db12317cbde631eb00bd5
kx <123@qq.com>
2022-01-14 00:33:08 +0800

    支付宝回调post

--- luffyapi/apps/order/urls.py
+++ luffyapi/apps/order/urls.py
@@ -7,4 +7,5 @@ router = SimpleRouter()
 router.register('pay', views.PayView, 'pay')
 urlpatterns = [
     path('', include(router.urls)),
+    path('success/',views.SuccessView.as_view()),
 ]
--- luffyapi/apps/order/views.py
+++ luffyapi/apps/order/views.py
@@ -7,6 +7,8 @@ from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
+from rest_framework.views import APIView
+
 
 # Create your views here.
 class PayView(GenericViewSet, CreateModelMixin):
@@ -30,3 +32,29 @@ class PayView(GenericViewSet, CreateModelMixin):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.context.get('pay_url'), status=201)
+
+
+class SuccessView(APIView):
+    """
+    支付宝回调 post
+    前端回调 get
+    """
+
+    def get(self, request, *args, **kwargs):
+        out_trade_no = request.query_params.get('out_trade_no')
+        order = models.Order.objects.filter(out_trade_no=out_trade_no).first()
+        if order.order_status == 1:
+            return Response('支付')
+        elif order.order_status == 0:
+            return Response('未支付')
+        return Response('后台收到')
+
+    def post(self, request, *args, **kwargs):
+        """
+        支付宝回调接口
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        ...
